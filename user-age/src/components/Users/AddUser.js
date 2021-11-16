5969f222d1ad4b96275c5f183929fac04e2edade
vishaljaddweb <vishal.j.addweb@gmail.com>
2021-11-16 23:00:04 +0530

    16/11/2021 updated from home

--- user-age/src/components/Users/AddUser.js
+++ user-age/src/components/Users/AddUser.js
@@ -40,9 +40,13 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value);
     }
 
+    const errorHandler =()=>{
+        setError(null);
+    }
+
     return (
         <div>
-            {error && <ErrorModal title={error.title} message={error.message} />}
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">UserName</label>
