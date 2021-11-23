a16850bd5277ec5b680e3f63532405d1abd88e8b
vishaljaddweb <vishal.j.addweb@gmail.com>
2021-11-23 19:04:00 +0530

    Task given by Kelvin Sir

---
+++ function-to-class-component-conversion/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import EventHandler from './functionalComponents/EventHandler';
+import UseState from './functionalComponents/UseState';
+
+function App() {
+  return (
+    <div>
+      {/* <EventHandler /> */}
+      <UseState />
+    </div>
+  );
+}
+
+export default App;
